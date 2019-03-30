558bf330c2567497cf5b18eea1d92fc45fc9c851
Cody <cody.tracy92@gmail.com>
2019-03-30 17:29:34 -0500

    added in the spotify and movie functions

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+var Spotify = require('node-spotify-api');
 require('dotenv').config()
 
 //Requiring the keys, and setting it to the keys variable.
@@ -9,38 +10,27 @@ function Spotify(id,secret){
     this.secret = secret;
 }
 
-var spotify = new Spotify(keys.spotify.id,keys.spotify.secret);
-
- 
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret}
+    );
 // spotify
-//   .search({ type: 'track', query: 'All the Small Things' })
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+
 
 var axios = require("axios");
 var moment = require('moment');
 moment().format();
 
-var searchParameter = ""
-
-function parameters (){
-    for (i = 3; i < process.argv.length; i++){
-        searchParameter = searchParameter + process.argv[i];
-    }
-}
-
 var command = process.argv[2];
+var searchParameter = process.argv.slice(3).join("+");
+var spotifySearch = process.argv.slice(3).join(" ");
+
 
 
 
 //Concert-this
 function concert(){
-    parameters();
-
+    
     if(searchParameter != ""){
         var queryUrl = "https://rest.bandsintown.com/artists/" + searchParameter + "/events?app_id=codingbootcamp"
 
@@ -74,18 +64,59 @@ function concert(){
     
 }
 
+function spotifyThis(){
+    if(spotifySearch == ""){
+        spotifySearch = "The Sign by Ace of Base"
+    }
+    console.log(spotifySearch + "*****");
+    spotify
+    .search({ type: 'track', query: spotifySearch })
+    .then(function(response) {
+        var response = response.tracks.items[0];
+        for(var i = 0; i<response.artists.length; i++){
+           console.log(response.artists[i].name); 
+        }
+      
+      console.log(response.name)
+      console.log(response.preview_url)
+      console.log(response.album.name)
+    //   console.log(response)
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+    
+}
+
+function movie(){
+    if (searchParameter == ""){
+        searchParameter = "Mr+Nobody"
+    }
+    var queryUrl = "https://www.omdbapi.com/?t=" + searchParameter + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(function(response){
+        var response = response.data;
+        console.log(response.Title);
+        console.log(response.Year);
+        console.log(response.imdbRating);
+        console.log(response.Ratings[1].Value);
+        console.log(response.Country);
+        console.log(response.Language);
+        console.log(response.Plot);
+        console.log(response.Actors);
+    })
+}
+
 
 function liri(){
   switch(command){
     case "concert-this": concert()
     break;
-    case "spotify-this-song": console.log('spotify not there yet')
+    case "spotify-this-song": spotifyThis()
     break;
-    case "movie-this": console.log("movie not there yet")
+    case "movie-this": movie()
     break;
     case "do-what-it-says": console.log("almost there but not yet")
     break;
 }  
 }
-
 liri();
